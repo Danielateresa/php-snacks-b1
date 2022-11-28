81660253b674b06c22d50a12f367253c40e257b0
Danielateresa <dany.t_87@hotmail.it>
2022-11-28 12:28:41 +0100

    x

--- snack-1/index.php
+++ snack-1/index.php
@@ -8,7 +8,33 @@ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squ
 Olimpia Milano - Cantù | 55-60 */
 
 $partite = [
-    $partita1 = "Olimpia Milano - Cantù" => "35 - 20",
-    $partita2 ="Bari - Roma" => "40 - 23",
-    $partita3 ="Torino -Napoli" => "25 - 40"
+    "partita1" => ["Olimpia Milano - Cantù", "35 - 20"],
+    "partita2" => ["Torino - Roma", "20 - 40"],
+    "partita3" => ["Napoli - Salerno", "35 - 25"],
 ];
+var_dump($partite);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h3>
+        <?php foreach ($partita as $partite) {
+            echo $partita;
+        } ?>
+    </h3>
+
+
+</body>
+
+</html>
